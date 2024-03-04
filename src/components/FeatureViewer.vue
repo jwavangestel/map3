d9d68e23c1f27ff70e028d5a46b02e92666760f0
Jasper van Gestel <jvangestel@gmail.com>
2024-03-04 22:04:11 +0100

    cleaned-up HomeView and added feature-viewer

---
+++ src/components/FeatureViewer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="feature-table">
+    <button type="button" @click="close">Close</button>
+    <table class="feature-table">
+      <thead>
+        <tr>
+          <th>
+            {{ feature.id }}
+          </th>
+          
+        </tr>
+      </thead>
+      <tr>
+        <th>
+          Gemeente
+        </th>
+        <td>
+          {{ properties.gemeente }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script setup>
+import { computed, defineProps } from 'vue';
+import { useFeatureStore } from "@/stores/featureStore";
+
+const props = defineProps({
+  feature: {
+    type: Object,
+    required: true,
+  },
+});
+
+const properties = computed(() => {
+  if ('properties' in props.feature) {
+    return props.feature.properties;
+  }
+  return null;
+});
+
+const featureStore = useFeatureStore();
+
+const close = (() => {
+  featureStore.resetCurrentFeature();
+});
+
+
+</script>
